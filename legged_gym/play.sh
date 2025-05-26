c56dedeea7c484367143fd42c9f592b167146ca1
lyc <15730060195@163.com>
2025-05-26 09:51:07 +0800

    first commit

---
+++ legged_gym/play.sh
@@ -0,0 +1,1 @@
+python3 scripts/play.py --task=g1 --load_run /home/ris/fdhumanoid_S2R/logs/FdBot_ppo/May23_17-33-40_v1 --num_envs 25
---
+++ legged_gym/train.sh
@@ -0,0 +1,1 @@
+python3 scripts/train.py --num_envs 5 --task=g1 --run_name v1
